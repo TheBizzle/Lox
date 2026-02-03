c5eb76ab07fdb75b04d2757de1d6f3e05184b22c
TheBizzle <jason.bertsche@gmail.com>
2026-02-02 20:26:10 -0600

    Infrastructure/Test: Switch to actual Lox test suite

--- README.md
+++ README.md
@@ -7,7 +7,33 @@ https://craftinginterpreters.com/
 
 ## Testing
 
-Install Python 3.12+ and pip.  Then, run `test.sh`.
+### Dart
+
+Install Dart 2.x:
+
+```sh
+wget https://storage.googleapis.com/dart-archive/channels/stable/release/2.19.6/sdk/dartsdk-linux-x64-release.zip
+unzip dartsdk-linux-x64-release.zip -d $HOME/Applications/dart-2.19.6
+export PATH="$HOME/Applications/dart-2.19.6/dart-sdk/bin:$PATH"
+```
+
+Make sure to add that last line to your `.zshrc` (or equivalent), for permanent use.
+
+### Building the tester
+
+```sh
+git clone https://github.com/munificent/craftinginterpreters.git mothership
+cd mothership
+make get
+make
+cd ..
+```
+
+### Running the tests
+
+```sh
+./test.sh
+```
 
 ## License
 
--- test.sh
+++ test.sh
@@ -2,28 +2,11 @@
 
 set -euo pipefail
 
-if ! command -v python3 &>/dev/null; then
-  echo "Python3 is required but not found.  Aborting...."
-  exit 1
-fi
-
-if ! command -v pip &>/dev/null; then
-  echo "pip is required but not found.  Aborting...."
-  exit 1
-fi
-
-if ! command -v loxtest &>/dev/null; then
-  echo "Installing loxygen...."
-  pip install --user loxygen
-fi
-
-if [ ! -d "lox-suite" ]; then
-  echo "Setting up Lox test suite...."
-  loxtest setup lox-suite
-fi
-
 echo "Building Haskell Lox interpreter...."
 stack build
 
 echo "Running Lox tests..."
-loxtest run --interpreter "stack exec -- lox" "lox-suite"
+(
+  cd ./mothership/ || exit 1
+  dart tool/bin/test.dart jlox --interpreter "../.stack-work/dist/x86_64-linux/ghc-9.10.3/build/lox/lox"\
+)
