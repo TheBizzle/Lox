6ed8786401d7f2dd9778e6f6b50027eba2afc288
TheBizzle <jason.bertsche@gmail.com>
2026-02-03 18:44:33 -0600

    Delete/Test: Remove typo from test script

--- test.sh
+++ test.sh
@@ -8,5 +8,5 @@ stack build
 echo "Running Lox tests..."
 (
   cd ./mothership/ || exit 1
-  dart tool/bin/test.dart jlox --interpreter "../.stack-work/dist/x86_64-linux/ghc-9.10.3/build/lox/lox"\
+  dart tool/bin/test.dart jlox --interpreter "../.stack-work/dist/x86_64-linux/ghc-9.10.3/build/lox/lox"
 )
