aae9d3e24c4064272243b4a3485230c824ff02d9
TheBizzle <jason.bertsche@gmail.com>
2026-02-08 17:21:08 -0600

    Feature: Enable verifier to navigate AST better

--- src/main/Lox/Verify/Internal/Verifier.hs
+++ src/main/Lox/Verify/Internal/Verifier.hs
@@ -10,6 +10,7 @@ import Lox.Parser.AST(
 
 import Lox.Verify.Internal.VerifierError(VerifierError(VerifierError), VerifierErrorType(DuplicateVar))
 
+import qualified Data.List          as List
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Set           as Set
 
@@ -22,38 +23,42 @@ data FnState =
 
 verify :: AST -> Validation (NonEmpty VerifierError) AST
 verify ast = ast.statements |>
-               (foldr (\x acc -> findErrorInStmt x <|> acc) Nothing) &>
+               (foldr (\x acc -> (evalState (findErrorInStmt x) (FnState Set.empty)) <|> acc) Nothing) &>
                maybe (Success ast) (NE.singleton &> Failure)
 
-findErrorInStmt :: Statement -> Maybe VerifierError
-findErrorInStmt (Block               _       ) = Nothing
-findErrorInStmt (Class               _  _   _) = Nothing
-findErrorInStmt (DeclareVar          _  _    ) = Nothing
-findErrorInStmt (ExpressionStatement _       ) = Nothing
-findErrorInStmt (Function            _ ps bod) = evalState (findErrorInFn ps bod) $ FnState Set.empty
-findErrorInStmt (IfElse              _  _   _) = Nothing
-findErrorInStmt (PrintStatement      _  _    ) = Nothing
-findErrorInStmt (ReturnStatement     _  _    ) = Nothing
-findErrorInStmt (WhileStatement      _  _    ) = Nothing
+findErrorInStmt :: Statement -> FnVerification
+findErrorInStmt (Block               _       ) = return Nothing
+findErrorInStmt (Class               _  _   _) = return Nothing
+findErrorInStmt (DeclareVar          _  _    ) = return Nothing
+findErrorInStmt (ExpressionStatement _       ) = return Nothing
+findErrorInStmt (Function            _ ps bod) = findErrorInFn ps bod
+findErrorInStmt (IfElse              _  _   _) = return Nothing
+findErrorInStmt (PrintStatement      _  _    ) = return Nothing
+findErrorInStmt (ReturnStatement     _  _    ) = return Nothing
+findErrorInStmt (WhileStatement      _  _    ) = return Nothing
 
 findErrorInFn :: [Variable] -> [Statement] -> FnVerification
 findErrorInFn vs stmts =
   do
-    result <- finder vs
+    let (_, result) = foldr func (Set.empty, Nothing) $ List.reverse vs
     if isJust result then
       return result
     else
       findErrorInStmts stmts
   where
-    finder    [] = return Nothing
-    finder (v:t) =
-      do
-        isDeclared <- gets $ vars &> Set.member v.varName
-        if isDeclared then
-          return $ Just $ VerifierError DuplicateVar v.varToken
-        else do
-          modify $ \fns -> fns { vars = Set.insert v.varName fns.vars }
-          finder t
+    func _ (names, (Just x)) = (names, Just x)
+    func v (names,        _) =
+      if Set.member v.varName names then
+        (names, Just $ VerifierError DuplicateVar v.varToken)
+      else
+        (Set.insert v.varName names, Nothing)
 
 findErrorInStmts :: [Statement] -> FnVerification
-findErrorInStmts _ = return Nothing
+findErrorInStmts    [] = return Nothing
+findErrorInStmts (h:t) =
+  do
+    result <- findErrorInStmt h
+    if isJust result then
+      return result
+    else
+      findErrorInStmts t
